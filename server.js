ab71c67e80dd7498d03e166b3c51e4b8af8b56a8
matthiasjrichter <127836330+matthiasjrichter@users.noreply.github.com>
2023-08-10 11:21:57 +0200

    tweaked locationCoordinates formatting

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ let locations = {
         "id": 5,
         "locationName": "Neues Rathaus",
         "locationAddress": "Werner-Senger-Straße 10",
-        "locationCoordinates": `50°23'12,5"N 8°3'40,2"O`,
+        "locationCoordinates": `50°23'12,5"N 8°3'40,2"E`,
         "locationDescription_de":`Das Neue Rathaus ist seit 1900 das Rathaus der mittelhessischen Kreisstadt Limburg an der Lahn. Seit spätestens 1502 wurde das Haus Fischmarkt 21 als Rathaus genutzt, dieses wurde jedoch im 19. Jahrhundert zu klein. Ab 1865 zog die Verwaltung in ein Schulgebäude am Neumarkt, bevor am 1. Dezember 1892 die Stadtverordnetenversammlung einen Neubau beschloss. Am 31. Oktober 1895 kaufte die Stadt schließlich ein Grundstück an der kürzlich erschlossenen Werner-Senger-Straße; 1897 wurden die Pläne fertiggestellt. Verantwortlich für den 100.000 Mark teuren Bau zeichneten der Wiesbadener Stadtbaumeister Felix Genzmer, dem der größere Anteil zugeschrieben wird, sowie der Limburger Stadtbaumeister Joseph Kauter. Laut Inschrift im Giebel wurde das Gebäude 1898 fertig gestellt, der Bezug erfolgte 1899 bis 1900.`,
         "wikipedia_de": "https://de.wikipedia.org/wiki/Neues_Rathaus_(Limburg_an_der_Lahn)"
     },
@@ -50,7 +50,7 @@ let locations = {
         "id": 6,
         "locationName": "Haus der sieben Laster",
         "locationAddress": "Brückengasse 9",
-        "locationCoordinates": `50°23'22,9"N 8°3'52,4"O`,
+        "locationCoordinates": `50°23'22,9"N 8°3'52,4"E`,
         "locationDescription_de":`Das Haus der sieben Laster ist ein denkmalgeschütztes Fachwerkhaus aus dem 16. Jahrhundert in der Altstadt von Limburg an der Lahn, dessen Fassade von Schnitzereien geziert wird, die allgemein hin als die sieben biblischen Laster Hochmut, Neid, Unmäßigkeit, Geiz, Wollust, Zorn und Trägheit gedeutet werden.`,
         "wikipedia_de": "https://de.wikipedia.org/wiki/Haus_der_sieben_Laster"
     },
@@ -58,7 +58,7 @@ let locations = {
         "id": 7,
         "locationName": "Römer 2-4-6",
         "locationAddress": "Römer 4",
-        "locationCoordinates": `50°23'20,9"N 8°3'52,2"O`,
+        "locationCoordinates": `50°23'20,9"N 8°3'52,2"E`,
         "locationDescription_de":`Römer 2-4-6 ist die Bezeichnung für ein gotisches Fachwerkhaus, dessen älteste verbaute Teile aus dem Jahr 1289 stammen. Damit ist der Römer 2-4-6 heute eines der ältesten, freistehenden Fachwerkhäuser in Deutschland und nationales Denkmal. Es steht in der historischen Altstadt von Limburg an der Lahn. Der Name leitet sich ab von der Straßenbezeichnung sowie den Hausnummern. Außerdem ist der Römer 2-4-6 das älteste Fachwerkhaus Limburgs.`,
         "wikipedia_de": "https://de.wikipedia.org/wiki/R%C3%B6mer_2-4-6"
     },
